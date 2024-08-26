423bb2a634df6c9090eb65894df2f1ccd26e39c5
kelvinkuria <kkuria383@yahoo.com>
2024-08-26 13:52:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(kit){
+cats.push(kit);
+}
+
+function appendCat(kit){
+    return [...cats,kit]
+}
